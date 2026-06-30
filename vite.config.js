ee1f076246f18e5e22f3de3d23113711f2c92eac
Елена <n.a.092024@yandex.ru>
2026-06-30 08:43:25 +0300

    fix: принудительная настройка outDir и base в vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: './',
+  build: {
+    outDir: 'dist', // принудительно говорим Vite складывать всё в dist
+  },
   plugins: [react(), tailwindcss(),],
 })
