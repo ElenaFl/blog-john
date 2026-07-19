3bac3ee81be5f3284b05d76720a7d232128f1815
Елена <n.a.092024@yandex.ru>
2026-07-20 01:14:36 +0300

    добавтить герб

--- src/components/ui/Footer/Footer.jsx
+++ src/components/ui/Footer/Footer.jsx
@@ -3,19 +3,29 @@ export const Footer = () => {
     <footer className="pt-52 pb-14 sm:pt-64 text-[#222222]">
       <div className="w-full sm:max-w-[858px] mx-auto">
         <div className="mb-6 flex justify-center items-center gap-6">
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="opacity-60 hover:opacity-100 transition-opacity">
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="opacity-60 hover:opacity-100 transition-opacity"
+          >
             <img
               src="/images/insta.svg"
               alt="Instagram"
               className="w-[18px] h-[18px] sm:w-[24px] sm:h-[24px] object-cover"
             />
           </a>
-           <img
-              src="/images/gerbP.png"
-              alt="gerb"
-              className="w-[20px] h-[20px] sm:w-[26px] sm:h-[26px] object-cover"
-            />
-          <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="opacity-60 hover:opacity-100 transition-opacity">
+          <img
+            src="/images/gerb.png"
+            alt="gerb"
+            className="w-[20px] h-[20px] sm:w-[26px] sm:h-[26px] object-cover"
+          />
+          <a
+            href="https://github.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="opacity-60 hover:opacity-100 transition-opacity"
+          >
             <img
               src="/images/github.jpg"
               alt="GitHub"
@@ -23,7 +33,7 @@ export const Footer = () => {
             />
           </a>
         </div>
-        
+
         <p className="text-[14px] text-center opacity-70">
           Copyright © {new Date().getFullYear()} All rights reserved
         </p>
